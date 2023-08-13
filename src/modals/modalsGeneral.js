2f7adb0460e11aaffd95de8f7e9a75e3050d0dca
Pavel <zvjxrf@yandex.ru>
2023-08-13 07:57:56 -0400

    payment method modal has been added

---
+++ src/modals/modalsGeneral.js
@@ -0,0 +1,49 @@
+'use strict'
+
+let _html = document.documentElement;
+export let scrollPosition;
+
+export function freezeHTML(){
+    let marginSize = window.innerWidth - _html.clientWidth;
+    _bodyScrollControl(marginSize);
+    if(_html.offsetWidth <= 480){
+        document.querySelector('.modals').style.top = _html.offsetHeight;
+    }
+    //сохраним текущую прокрутку:
+    scrollPosition = window.pageYOffset;
+    _html.style.top = -scrollPosition  + "px";
+    _html.classList.add("hystmodal__opened");
+
+    let turnBackgroundOn = _toggleModalBackground(true);
+    turnBackgroundOn(marginSize);
+}
+
+export function unfreezeHTML(){
+    _html.classList.remove("hystmodal__opened");
+    window.scrollTo(0, scrollPosition);
+    _html.style.top = "";
+
+    _bodyScrollControl(0);
+    _html.style.marginRight = "";
+
+    let turnBackgroundOff = _toggleModalBackground(false);
+    turnBackgroundOff('');
+}
+
+function _bodyScrollControl(marginSize){
+    if (marginSize) {
+        _html.style.marginRight = marginSize + "px";
+    }
+    document.querySelector('.content-main').style.paddingRight = marginSize + "px"
+    document.querySelector('header').style.paddingRight = marginSize + "px"
+    document.querySelector('footer').style.paddingRight = marginSize + "px"
+}
+
+function _toggleModalBackground(turn){
+    return (marginSize) => {
+        document.querySelector('.modals-background').style.top = scrollPosition + 'px'
+        document.querySelector('.modals-background').style.paddingRight = marginSize + 'px'
+        turn ? document.querySelector('.modals-background').style.display = 'block'
+            : document.querySelector('.modals-background').style.display = 'none'
+    }
+}
---
+++ src/modals/paymentMethodModal.js
@@ -0,0 +1,51 @@
+'use strict'
+import {freezeHTML, unfreezeHTML, scrollPosition} from './modalsGeneral.js'
+
+//  IMPORTANT !!
+//
+// We don't have, contain and process the data of cards yet.
+// that means - events change html directly and don't save the changes or the data
+
+// init
+document.getElementById('card-mir').checked = true;
+
+
+
+function openPaymentModal(){
+    freezeHTML();
+
+    document.documentElement.offsetWidth <= 480
+        ? document.querySelector('.payment-method-modal').style.display = 'grid'
+        : document.querySelector('.payment-method-modal').style.display = 'block'
+    document.querySelector('.payment-method-modal').style.top = scrollPosition + 'px';
+}
+function closePaymentModal(){
+    unfreezeHTML();
+    document.querySelector('.payment-method-modal').style.display = 'none'
+}
+
+
+// click event to open payment-method-modal
+document.querySelectorAll('.payment-method-edit').forEach(edit => {
+    edit.addEventListener('click', (e) => {
+        openPaymentModal();
+    })
+})
+
+// click event to close payment-method-modal
+document.querySelector('.payment-method-modal-title-close').addEventListener('click', (e) => {
+    closePaymentModal();
+})
+
+
+// click event to set selected card
+// We don't have, contain and process the data of cards yet.
+// this event only changes and sets the image in html-element directly
+document.querySelector('.choose-card-button').addEventListener('click', (e) => {
+    let selectedCard = Array.prototype.slice.call(document.querySelectorAll('.card-item input')).filter(item => item.checked);
+    document.querySelectorAll('.card-info .card-background span').forEach(card => {
+        card.classList.remove(`${card.className}`);
+        card.classList.add(`${selectedCard[0].id}-icon`)
+    })
+    closePaymentModal();
+})
